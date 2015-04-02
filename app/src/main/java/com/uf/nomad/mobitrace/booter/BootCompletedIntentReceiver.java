8d3dccdfb9b2e4cfdab4cacca4418157a4e2f61d
Babak Alipour <babak.alipour@gmail.com>
2015-04-01 20:00:45 -0400

    -booter changes

--- app/src/main/java/com/uf/nomad/mobitrace/booter/BootCompletedIntentReceiver.java
+++ app/src/main/java/com/uf/nomad/mobitrace/booter/BootCompletedIntentReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.uf.nomad.mobitrace.LocationUpdateService;
+
 /**
  * Created by Babak on 4/1/2015.
  */
@@ -12,8 +14,11 @@ public class BootCompletedIntentReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
             //TODO: BackgroundService.class should be implemented
-//            Intent pushIntent = new Intent(context, BackgroundService.class);
-//            context.startService(pushIntent);
+            Intent pushIntent1 = new Intent(context, LocationUpdateService.class);
+            context.startService(pushIntent1);
+
+//            Intent pushIntent2 = new Intent(context,activity class);
+//            context.startService(pushIntent2);
         }
     }
 }
