01b6ded24633044ad2aac8e1ffc1b6ffc997bb1e
Babak Alipour <babak.alipour@gmail.com>
2015-08-04 02:10:03 -0400

    -Tag warning fixed

--- app/src/main/java/com/uf/nomad/mobitrace/PowerConnectionBroadcastReceiver.java
+++ app/src/main/java/com/uf/nomad/mobitrace/PowerConnectionBroadcastReceiver.java
@@ -17,14 +17,14 @@ public class PowerConnectionBroadcastReceiver extends BroadcastReceiver {
         int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
         boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
                 status == BatteryManager.BATTERY_STATUS_FULL;
-        Log.e("POWERCONNECTIONBROADCASTRECEIVER", "charging: " + isCharging);
+        Log.e("PowerBCR", "charging: " + isCharging);
 
         //charging via AC or USB
         int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
         boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        Log.e("POWERCONNECTIONBROADCASTRECEIVER", "usb plugged: " + usbCharge);
+        Log.e("PowerBCR", "usb plugged: " + usbCharge);
         boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-        Log.e("POWERCONNECTIONBROADCASTRECEIVER", "AC plugged: " + acCharge);
+        Log.e("PowerBCR", "AC plugged: " + acCharge);
 
 
         //battery remaining percentage
